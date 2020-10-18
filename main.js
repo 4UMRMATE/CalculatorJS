a4940dec32b2f0a13f66486576a854b2e2220c46
4UMRMATE <sich.mate@gmail.com>
2020-10-18 14:40:18 +0400

    -- fixed spacing

--- main.js
+++ main.js
@@ -13,25 +13,26 @@ clickables.forEach((elem) => {
             l = theNum.length,
             lth;
 
-        function Animations() {     // button animations ( regular ones & for operator buttons)
-
+        function Animations() {    // button animations ( regular ones & for operator buttons)
+            
             function DeleteOprStyle() {
                 let i = 0;
-                while(i < 4){
+
+                while (i < 4){
                     let oprStyle = document.querySelectorAll("#calc-opr")[i];
                     oprStyle.style = "";
                     ++i;
                 }
             }
 
-            if(id !== "calc-opr") {
+            if (id !== "calc-opr") {
                 DeleteOprStyle();
                 elem.style.transition = "0.15s"
                 elem.style.opacity = "0.5"
                 setTimeout(function(){
                     elem.style.opacity = "1"
                 },150)         
-            }else {
+            } else {
                 DeleteOprStyle();
                 elem.style.backgroundColor = "#909090";
                 elem.style.color = "white";
@@ -40,21 +41,22 @@ clickables.forEach((elem) => {
         Animations();
 
         function Display() {
+            if (id == "calc-num" || id == "calc-zero") {
+                if (buttonValue === "." && res.value.includes(".")) return;
 
-            if(buttonValue === "." && res.value.includes(".")) return;
-
-            if (res.value == "") {
-                theNum = buttonValue;
-              }else if(l < 9) {
-                theNum += buttonValue;
-              }
+                if (res.value == "") {
+                    theNum = buttonValue;
+                } else if (l < 9) {
+                    theNum += buttonValue;
+                }
             
-              res.value = theNum;
+                res.value = theNum;
+            }     
         }
-        if(id == "calc-num" || id == "calc-zero") Display();
+        Display();
 
         function Operator() {
-            if(id == "calc-opr"){
+            if (id == "calc-opr"){
                 holder.value = res.value + " " + buttonValue + " ";
                 oldNum = res.value;
                 theNum = "";
@@ -63,43 +65,44 @@ clickables.forEach((elem) => {
         }
         Operator();
 
-        function Calculate(oldNum) {
-
-            switch (operator) {
-                case "+":
-                  res.value = +oldNum + +theNum;
-                  break;
+        function Calculate() {
+            if (id == "calc-equ") {
+                switch (operator) {
+                    case "+":
+                      res.value = +oldNum + +theNum;
+                      break;
           
-                case "-":
-                  res.value = +oldNum - +theNum;
-                  break;
+                    case "-":
+                      res.value = +oldNum - +theNum;
+                      break;
           
-                case "×":
-                  res.value = +oldNum * +theNum;
-                  break;
+                    case "×":
+                      res.value = +oldNum * +theNum;
+                      break;
           
-                case "÷":
-                  res.value= +oldNum / +theNum;
-                  break;
+                    case "÷":
+                      res.value= +oldNum / +theNum;
+                      break;
                 
-                default:
-                  resultNum = theNum;
-              }
+                    default:
+                      resultNum = theNum;
+                }
 
-            holder.value = res.value;
+                holder.value = res.value;
+            }   
         }
-        if(id == "calc-equ") Calculate(oldNum);
+        Calculate();
 
-        let Sqrt = function() {
-            if(buttonValue === "√") {
-                if(res.value >= 0){
+        function Sqrt() {
+            if (buttonValue === "√") {
+                if (res.value >= 0){
                     res.value = Math.sqrt(res.value);
-                }else {
+                } else {
                     alert("negative number!")
                 }
                 holder.value = res.value;
                 lth = res.value.toString().length;
-                if(lth > 9) {
+                if (lth > 9) {
                     res.style.fontSize = "50px";
                     holder.style.fontSize = "25px";
                 }
@@ -108,16 +111,16 @@ clickables.forEach((elem) => {
         Sqrt();
 
         function Clear() {
-            if(buttonValue == "AC") {
+            if (buttonValue == "AC") {
                 holder.value = res.value = "";  
                 res.style.fontSize = "100px";
                 holder.style.fontSize ="50px";
-            }else if(buttonValue == "C") {
+            } else if (buttonValue == "C") {
                 res.value = res.value.slice(0, -1);
                 theNum = theNum.slice(0, -1);
-                }
+            }
         }
         Clear();
 
-    });
-})
+    }); // clickButton()
+})      // clickables.forEach
